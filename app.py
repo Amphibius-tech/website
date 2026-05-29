f36134fd128cb2277e9e8ca930a2ea8b1dded113
amphibiustech <amphibiustech@gmail.com>
2026-05-29 12:15:25 +0530

    new template

--- app.py
+++ app.py
@@ -1,46 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from src.db_provider.db_service import db, EnquiryForm
+
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-app.config['SECRET_KEY'] = "SecREtKeY"
 
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('main.html')
 
-@app.route('/form', methods=['POST'])
-def form():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    service = request.form.get('service')
-    message = request.form.get('message')
+# @app.route('/form', methods=['POST'])
+# def form():
+#     name = request.form.get('name')
+#     email = request.form.get('email')
+#     service = request.form.get('service')
+#     message = request.form.get('message')
 
-    if not all([name, email, service, message]):
-        flash("Please fill in all required fields.", "warning")
-        return redirect(url_for('index'))
+#     if not all([name, email, service, message]):
+#         flash("Please fill in all required fields.", "warning")
+#         return redirect(url_for('index'))
 
-    try:
-        new_form = EnquiryForm(
-            name=name,
-            email=email,
-            service=service,
-            message=message
-        )
-        db.session.add(new_form)
-        db.session.commit()
-        flash("✅ Your enquiry has been submitted successfully!", "success")
-    except Exception as e:
-        db.session.rollback()
-        flash("❌ Something went wrong. Please try again later.", "danger")
-        print("Error:", e)
+#     try:
+#         new_form = EnquiryForm(
+#             name=name,
+#             email=email,
+#             service=service,
+#             message=message
+#         )
+#         db.session.add(new_form)
+#         db.session.commit()
+#         flash("✅ Your enquiry has been submitted successfully!", "success")
+#     except Exception as e:
+#         db.session.rollback()
+#         flash("❌ Something went wrong. Please try again later.", "danger")
+#         print("Error:", e)
     
-    return redirect(url_for('index'))
+#     return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
