71c3b20c55b8302fccf37832313542f901e06168
amphibiustech <amphibiustech@gmail.com>
2026-06-04 17:44:37 +0530

    logs added

--- src/routes/landing.py
+++ src/routes/landing.py
@@ -1,27 +1,36 @@
 from flask import Blueprint, render_template, request, jsonify, url_for
 
 from src.database.provider.leads_info import LeadsInfoProvider
+from src.util.log_adapter import logger
 
 landing_bp = Blueprint("landing_bp", __name__, static_folder="static", template_folder="templates")
 
 
 @landing_bp.route('/', methods=['GET'])
 def index():
-    return render_template('main.html')
+    try:
+        return render_template('main.html')
+    except Exception as e:
+        logger.error(f"Error rending the main page: {str(e)}")
 
 @landing_bp.route("/submit-enquiry", methods=["POST"])
 def submit_enquiry():
+    try:
+        LeadsInfoProvider().add_leads_entry(request)
 
-    LeadsInfoProvider().add_leads_entry(request)
-
-    return jsonify({
-        "status": "success",
-        "message": "Enquiry submitted successfully"
-    }), 200
+        return jsonify({
+            "status": "success",
+            "message": "Enquiry submitted successfully"
+        }), 200
+    except Exception as e:
+        logger.error(f"Error submit_enquiry the main page: {str(e)}")
     
 @landing_bp.route("/signin", methods=["GET"])
 def signin():
-    return render_template("signin.html")
+    try:
+        return render_template("signin.html")
+    except Exception as e:
+        logger.error(f"Error signin the main page: {str(e)}")
 
 @landing_bp.route("/login", methods=["POST"])
 def login():
@@ -43,6 +52,7 @@ def login():
                     "message": "Invalid email or password"
                 }), 401
     except Exception as e:
+        logger.error(f"Error login the main page: {str(e)}")
         return jsonify({
             "status": "error",
             "message": str(e)
